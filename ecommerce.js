f0d2c6b6faec99ed1ac2b2278d866f4202649310
Mayank_MP5 <mayank5pande@gmail.com>
2020-08-04 17:37:43 +0530

    Default Schema Added

--- ecommerce.js
+++ ecommerce.js
@@ -1,15 +1,49 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-
+const items = require("./data/items.json");
+const companies = require("./data/companies.json");
 const {
   GraphQLString,
   GraphQLBoolean,
   GraphQLObjectType,
   GraphQLInt,
+  GraphQLNonNull,
+  GraphQLSchema,
 } = require("graphql");
 
 const app = express();
-app.use("/", (req, res) => {
+/*
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represents a book written by an author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      resolve: (book) => {
+        return authors.find((author) => author.id === book.authorId);
+      },
+    },
+  }),
+});
+
+
+*/
+//Default Schema
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "DemoSchema",
+    description: "Used Just for the First Start",
+    fields: () => ({
+      testmsg: { type: GraphQLString, resolve: () => "Demo Schema Output" },
+    }),
+  }),
+});
+
+app.get("/", (req, res) => {
   res.redirect("/graphql");
 });
 app.use(
@@ -19,4 +53,4 @@ app.use(
     graphiql: true,
   })
 );
-app.listen(5000, () => console.log("Server Running"));
+app.listen(5000, () => console.log("Server Running at 5000"));
